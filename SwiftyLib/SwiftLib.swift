48a8f1d6de792cbebd17dba4b9dadae473f041c2
Dheeraj Kumar <dheerajkmar@gmail.com>
2020-07-22 11:37:32 +0530

    Added test cases

---
+++ SwiftyLib/SwiftLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftLib.swift
+//  SwiftyLib
+//
+//  Created by Dheeraj Kumar on 22/07/20.
+//  Copyright © 2020 Conduent. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
